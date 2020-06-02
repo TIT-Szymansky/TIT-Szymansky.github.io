2b77e1524ec05e29e7924ecd1d40266eff0efea1
TIT-Szymansky <64528838+TIT-Szymansky@users.noreply.github.com>
2020-06-03 01:39:03 +0200

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,73 @@
+function zmien1()
+{
+	document.getElementById('1').src="fotojd.png";
+}
+function zmien2()
+{
+	document.getElementById('1').src="foto1.png";
+}
+function zmien3 ()
+{	
+		document.getElementById('2').src="fotojd.png";
+}
+function zmien4 ()
+{
+		document.getElementById('2').src="foto2.png";
+}
+function zmien5 ()
+{
+	document.getElementById('3').src="fotojd.png";
+}
+function zmien6 ()
+{
+	document.getElementById('3').src="foto3.png";
+}
+function zmien7 ()
+{
+	document.getElementById('4').src="fotojd.png";
+}
+function zmien8 ()
+{
+	document.getElementById('4').src="foto4.png";
+}
+function zmien9 ()
+{
+	document.getElementById('5').src="fotojd.png";
+}
+function zmien10 ()
+{
+	document.getElementById('5').src="foto5.png";
+}
+function zmien11 ()
+{
+	document.getElementById('6').src="fotojd.png";
+}
+function zmien12 ()
+{
+	document.getElementById('6').src="foto6.png";
+}
+function zmien13 ()
+{
+	document.getElementById('7').src="fotojd.png";
+}
+function zmien14 ()
+{
+	document.getElementById('7').src="foto7.png";
+}
+function zmien15 ()
+{
+	document.getElementById('8').src="fotojd.png";
+}
+function zmien16 ()
+{
+	document.getElementById('8').src="foto8.png";
+}
+function zmien17 ()
+{
+	document.getElementById('9').src="fotojd.png";
+}
+function zmien18 ()
+{
+	document.getElementById('9').src="foto9.png";
+}
+
